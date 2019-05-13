98cb4be8ba26470207d852f84bc818b656b26cd0
Natalie J <natsetc@gmail.com>
2019-05-13 11:27:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,12 @@
 def dictionary
   w_and_s = 
   {
-    "hello" => 'hi'
-"to, two, too" become '2' 
-"for, four" become '4'
-'be' becomes 'b'
-'you' becomes 'u'
-"at" becomes "@" 
-"and" becomes "&"
+    "hello" => 'hi',
+  "to, two, too" => '2',
+  "for, four" => '4',
+  'be' => 'b',
+'you' => 'u',
+"at" => "@" ,
+"and" => "&"
   }
 end
