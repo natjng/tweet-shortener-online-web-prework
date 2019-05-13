4a1894a06a5e34b259cbd095de9ff7d86a4ac349
Natalie J <natsetc@gmail.com>
2019-05-13 11:28:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,11 @@
 def dictionary
-  w_and_s = 
-  {
+  w_and_s = {
     "hello" => 'hi',
-  "to, two, too" => '2',
-  "for, four" => '4',
-  'be' => 'b',
-'you' => 'u',
-"at" => "@" ,
-"and" => "&"
+    "to, two, too" => '2',
+    "for, four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@" ,
+    "and" => "&"
   }
 end
